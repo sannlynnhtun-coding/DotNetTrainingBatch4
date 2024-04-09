9053c186e571d21a752f13be344d5a17ddb2b523
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2024-04-09 22:57:08 +0630

    [Complete] Ado.Net Read

--- DotNetTrainingBatch4.ConsoleApp/Program.cs
+++ DotNetTrainingBatch4.ConsoleApp/Program.cs
@@ -1,1 +1,55 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System.Data;
+using System.Data.SqlClient;
+
+Console.WriteLine("Hello, World!");
+
+// npm
+// pub.dev
+// nuget
+//SqlConnection
+
+// Ctrl + . => suggestion
+// F10 => 
+// F11
+// F9 => Breakpoint
+
+// => C# => Db
+
+SqlConnectionStringBuilder stringBuilder = new SqlConnectionStringBuilder();
+stringBuilder.DataSource = "."; // server name
+stringBuilder.InitialCatalog = "DotNetTrainingBatch4"; // database name
+stringBuilder.UserID = "sa";
+stringBuilder.Password = "sasa@123";
+SqlConnection connection = new SqlConnection(stringBuilder.ConnectionString);
+//SqlConnection connection = new SqlConnection("Data Source=USER;Initial Catalog=DotNetTrainingBatch4;User ID=sa;Password=sasa@123");
+
+connection.Open();
+Console.WriteLine("Connection open.");
+
+string query = "select * from tbl_blog";
+SqlCommand cmd = new SqlCommand(query, connection);
+SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(cmd);
+DataTable dt = new DataTable();
+sqlDataAdapter.Fill(dt);
+
+connection.Close();
+Console.WriteLine("Connection close.");
+
+// dataset => datatable
+// datatable => datarow
+// datarow => datacolumn
+
+foreach (DataRow dr in dt.Rows)
+{
+    Console.WriteLine("Blog Id => " + dr["BlogId"]);
+    Console.WriteLine("Blog Tilte => " + dr["BlogTitle"]);
+    Console.WriteLine("Blog Author => " + dr["BlogAuthor"]);
+    Console.WriteLine("Blog Content => " + dr["BlogContent"]);
+    Console.WriteLine("--------------------------------");
+}
+
+
+// Ado.Net Read
+// CRUD
+
+Console.ReadKey();
---
+++ README.md
@@ -0,0 +1,9 @@
+UI + Business Logic + Data Access + Db
+
+Mobile, Web => API => Database
+
+C# => Db
+
+SLHDotNetCore
+> SLHDotNetCore.ConsoleApp
+> SLHDotNetCore.RestApi
