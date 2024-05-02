84b743f7ff6190d1d588264292ab861fbdfe4b8d
Zin Linn Htet <mackk5504@gmail.com>
2024-05-02 23:52:50 +0630

    InProgress BlogAdo Controller

--- DotNetTrainingBatch4.Shared/AdoDotNetService.cs
+++ DotNetTrainingBatch4.Shared/AdoDotNetService.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.SqlClient;
+﻿using System.Data.SqlClient;
 using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Newtonsoft.Json;
 
 namespace DotNetTrainingBatch4.Shared
