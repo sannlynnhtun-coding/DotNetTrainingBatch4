53204ec70199731c5ed08cfa9c633be14634887f
Zin Linn Htet <mackk5504@gmail.com>
2024-05-04 00:23:32 +0630

    -

--- DotNetTrainingBatch4.RestApi/GlobalUsings.cs
+++ DotNetTrainingBatch4.RestApi/GlobalUsings.cs
@@ -2,5 +2,8 @@
 
 global using System.Data;
 global using System.Data.SqlClient;
+global using Dapper;
+global using DotNetTrainingBatch4.RestApi.Db;
 global using DotNetTrainingBatch4.RestApi.Models;
+global using DotNetTrainingBatch4.Shared;
 global using Microsoft.AspNetCore.Mvc;
