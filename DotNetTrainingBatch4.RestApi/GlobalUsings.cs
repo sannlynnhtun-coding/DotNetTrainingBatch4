d82af6cfeb8569690200b5b1c019427eb1f1c79c
Zin Linn Htet <mackk5504@gmail.com>
2024-05-04 00:20:41 +0630

    -

---
+++ DotNetTrainingBatch4.RestApi/GlobalUsings.cs
@@ -0,0 +1,6 @@
+﻿// Global using directives
+
+global using System.Data;
+global using System.Data.SqlClient;
+global using DotNetTrainingBatch4.RestApi.Models;
+global using Microsoft.AspNetCore.Mvc;
