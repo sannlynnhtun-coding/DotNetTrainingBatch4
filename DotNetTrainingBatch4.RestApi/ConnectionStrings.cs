f4e143d9bf553f04324439d06babd4efad4ec442
Zin Linn Htet <mackk5504@gmail.com>
2024-05-03 23:28:27 +0630

    -

--- DotNetTrainingBatch4.RestApi/ConnectionStrings.cs
+++ DotNetTrainingBatch4.RestApi/ConnectionStrings.cs
@@ -7,7 +7,8 @@ internal static class ConnectionStrings
     public static SqlConnectionStringBuilder SqlConnectionStringBuilder = new SqlConnectionStringBuilder()
     {
         DataSource = ".",
-        InitialCatalog = "DotNetTrainingBatch4",
+        // InitialCatalog = "DotNetTrainingBatch4",
+        InitialCatalog = "TestDb",
         UserID = "sa",
         Password = "sasa@123",
         TrustServerCertificate = true
