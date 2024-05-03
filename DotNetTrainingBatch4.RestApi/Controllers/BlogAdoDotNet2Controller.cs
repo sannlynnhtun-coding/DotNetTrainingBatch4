2654b97e75440fd05ee6a2e535ba7bb14edec2f0
Zin Linn Htet <mackk5504@gmail.com>
2024-05-04 00:23:11 +0630

    -

--- DotNetTrainingBatch4.RestApi/Controllers/BlogAdoDotNet2Controller.cs
+++ DotNetTrainingBatch4.RestApi/Controllers/BlogAdoDotNet2Controller.cs
@@ -1,57 +1,55 @@
-﻿using Microsoft.AspNetCore.Http;
-using DotNetTrainingBatch4.Shared;
+﻿namespace DotNetTrainingBatch4.RestApi.Controllers;
 
-namespace DotNetTrainingBatch4.RestApi.Controllers
+[Route("api/[controller]")]
+[ApiController]
+public class BlogAdoDotNet2Controller : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class BlogAdoDotNet2Controller : ControllerBase
+    private readonly AdoDotNetService _adoDotNetService =
+        new AdoDotNetService(ConnectionStrings.SqlConnectionStringBuilder.ConnectionString);
+
+    [HttpGet]
+    public IActionResult GetBlogs()
     {
-        private readonly AdoDotNetService _adoDotNetService = new AdoDotNetService(ConnectionStrings.SqlConnectionStringBuilder.ConnectionString);
+        string query = "select * from tbl_blog";
+        var lst = _adoDotNetService.Query<BlogModel>(query);
 
-        [HttpGet]
-        public IActionResult GetBlogs()
+        return Ok(lst);
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult GetBlog(int id)
+    {
+        string query = "select * from tbl_blog where BlogId = @BlogId";
+
+        //AdoDotNetParameter[] parameters = new AdoDotNetParameter[1];
+        //parameters[0] = new AdoDotNetParameter("@BlogId", id);
+        //var lst = _adoDotNetService.Query<BlogModel>(query, parameters);
+
+        var item = _adoDotNetService.QueryFirstOrDefault<BlogModel>(query, new AdoDotNetParameter("@BlogId", id));
+
+        //SqlConnection connection = new SqlConnection(ConnectionStrings.SqlConnectionStringBuilder.ConnectionString);
+        //connection.Open();
+
+        //SqlCommand cmd = new SqlCommand(query, connection);
+        //cmd.Parameters.AddWithValue("@BlogId", id);
+        //SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(cmd);
+        //DataTable dt = new DataTable();
+        //sqlDataAdapter.Fill(dt);
+
+        //connection.Close();
+
+        if (item is null)
         {
-            string query = "select * from tbl_blog";
-            var lst = _adoDotNetService.Query<BlogModel>(query);
-
-            return Ok(lst);
+            return NotFound("No data found.");
         }
 
-        [HttpGet("{id}")]
-        public IActionResult GetBlog(int id)
-        {
-            string query = "select * from tbl_blog where BlogId = @BlogId";
+        return Ok(item);
+    }
 
-            //AdoDotNetParameter[] parameters = new AdoDotNetParameter[1];
-            //parameters[0] = new AdoDotNetParameter("@BlogId", id);
-            //var lst = _adoDotNetService.Query<BlogModel>(query, parameters);
-
-            var item = _adoDotNetService.QueryFirstOrDefault<BlogModel>(query, new AdoDotNetParameter("@BlogId", id));
-
-            //SqlConnection connection = new SqlConnection(ConnectionStrings.SqlConnectionStringBuilder.ConnectionString);
-            //connection.Open();
-
-            //SqlCommand cmd = new SqlCommand(query, connection);
-            //cmd.Parameters.AddWithValue("@BlogId", id);
-            //SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(cmd);
-            //DataTable dt = new DataTable();
-            //sqlDataAdapter.Fill(dt);
-
-            //connection.Close();
-
-            if (item is null)
-            {
-                return NotFound("No data found.");
-            }
-
-            return Ok(item);
-        }
-
-        [HttpPost]
-        public IActionResult CreateBlog(BlogModel blog)
-        {
-            string query = @"INSERT INTO [dbo].[Tbl_Blog]
+    [HttpPost]
+    public IActionResult CreateBlog(BlogModel blog)
+    {
+        string query = @"INSERT INTO [dbo].[Tbl_Blog]
            ([BlogTitle]
            ,[BlogAuthor]
            ,[BlogContent])
@@ -60,15 +58,14 @@ namespace DotNetTrainingBatch4.RestApi.Controllers
            ,@BlogAuthor       
            ,@BlogContent)";
 
-            int result = _adoDotNetService.Execute(query,
-                new AdoDotNetParameter("@BlogTitle", blog.BlogTitle),
-                new AdoDotNetParameter("@BlogAuthor", blog.BlogAuthor),
-                new AdoDotNetParameter("@BlogContent", blog.BlogContent)
-            );
+        int result = _adoDotNetService.Execute(query,
+            new AdoDotNetParameter("@BlogTitle", blog.BlogTitle),
+            new AdoDotNetParameter("@BlogAuthor", blog.BlogAuthor),
+            new AdoDotNetParameter("@BlogContent", blog.BlogContent)
+        );
 
-            string message = result > 0 ? "Saving Successful." : "Saving Failed.";
-            //return StatusCode(500, message);
-            return Ok(message);
-        }
+        string message = result > 0 ? "Saving Successful." : "Saving Failed.";
+        //return StatusCode(500, message);
+        return Ok(message);
     }
 }
