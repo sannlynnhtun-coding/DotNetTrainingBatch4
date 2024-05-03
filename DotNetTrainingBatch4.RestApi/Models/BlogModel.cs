d525620af80c57d4fb67741da2456fbf31220f38
Zin Linn Htet <mackk5504@gmail.com>
2024-05-04 00:20:29 +0630

    -

--- DotNetTrainingBatch4.RestApi/Models/BlogModel.cs
+++ DotNetTrainingBatch4.RestApi/Models/BlogModel.cs
@@ -3,7 +3,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace DotNetTrainingBatch4.RestApi.Models;
 
-[Table("Tbl_Blogs")]
+[Table("Tbl_Blog")]
 public class BlogModel
 {
     [Key]
