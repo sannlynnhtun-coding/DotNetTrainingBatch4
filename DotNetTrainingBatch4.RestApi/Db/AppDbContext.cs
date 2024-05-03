a5cb8b57bf4282bf31d033cc0c20768905e72b67
Zin Linn Htet <mackk5504@gmail.com>
2024-05-03 23:26:54 +0630

    -

--- DotNetTrainingBatch4.RestApi/Db/AppDbContext.cs
+++ DotNetTrainingBatch4.RestApi/Db/AppDbContext.cs
@@ -1,20 +1,13 @@
 ﻿using DotNetTrainingBatch4.RestApi.Models;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+namespace DotNetTrainingBatch4.RestApi.Db;
 
-namespace DotNetTrainingBatch4.RestApi.Db
+internal class AppDbContext : DbContext
 {
-    internal class AppDbContext : DbContext
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseSqlServer(ConnectionStrings.SqlConnectionStringBuilder.ConnectionString);
-        }
-
-        public DbSet<BlogModel> Blogs { get; set; }
+        optionsBuilder.UseSqlServer(ConnectionStrings.SqlConnectionStringBuilder.ConnectionString);
     }
+
+    public DbSet<BlogModel> Blogs { get; set; }
 }
