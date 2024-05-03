cd536df817103410a7bdf2787a61426e0bd4e24e
Zin Linn Htet <mackk5504@gmail.com>
2024-05-04 00:20:18 +0630

    -

--- DotNetTrainingBatch4.RestApi/Controllers/BlogDapper2Controller.cs
+++ DotNetTrainingBatch4.RestApi/Controllers/BlogDapper2Controller.cs
@@ -1,10 +1,6 @@
 ﻿using Dapper;
-using DotNetTrainingBatch4.RestApi.Models;
 using DotNetTrainingBatch4.Shared;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using System.Data;
-using System.Data.SqlClient;
 using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
 
 namespace DotNetTrainingBatch4.RestApi.Controllers
