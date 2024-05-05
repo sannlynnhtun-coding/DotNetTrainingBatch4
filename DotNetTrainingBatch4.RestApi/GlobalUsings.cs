4d0980d150f0d275bc977e4cf41926248b0fdf4f
Zin Linn Htet <mackk5504@gmail.com>
2024-05-05 21:29:51 +0630

    Complete Delete and Put

--- DotNetTrainingBatch4.RestApi/GlobalUsings.cs
+++ DotNetTrainingBatch4.RestApi/GlobalUsings.cs
@@ -1,5 +1,7 @@
 ﻿// Global using directives
 
+global using System.ComponentModel.DataAnnotations;
+global using System.ComponentModel.DataAnnotations.Schema;
 global using System.Data;
 global using System.Data.SqlClient;
 global using Dapper;
