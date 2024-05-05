40545de39114df5ae2e92a96a5ab69e0a18d822b
Zin Linn Htet <mackk5504@gmail.com>
2024-05-05 21:29:47 +0630

    Complete Delete and Put

--- DotNetTrainingBatch4.RestApi/ConnectionStrings.cs
+++ DotNetTrainingBatch4.RestApi/ConnectionStrings.cs
@@ -5,8 +5,8 @@ internal static class ConnectionStrings
     public static SqlConnectionStringBuilder SqlConnectionStringBuilder = new SqlConnectionStringBuilder()
     {
         DataSource = ".",
-        InitialCatalog = "DotNetTrainingBatch4",
-        // InitialCatalog = "TestDb",
+        // InitialCatalog = "DotNetTrainingBatch4",
+        InitialCatalog = "TestDb",
         UserID = "sa",
         Password = "sasa@123",
         TrustServerCertificate = true
