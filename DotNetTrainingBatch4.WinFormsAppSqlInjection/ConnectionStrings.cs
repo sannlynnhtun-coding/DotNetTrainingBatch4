676f555ca57b6b1a8323be31e1750e30009bc016
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2024-05-28 22:59:26 +0630

    [Complete] Sql Injection

---
+++ DotNetTrainingBatch4.WinFormsAppSqlInjection/ConnectionStrings.cs
@@ -0,0 +1,16 @@
+﻿using System.Data.SqlClient;
+
+namespace DotNetTrainingBatch4.WinFormsAppSqlInjection;
+
+internal static class ConnectionStrings
+{
+    public static SqlConnectionStringBuilder sqlConnectionStringBuilder = new SqlConnectionStringBuilder()
+    {
+        DataSource = ".",
+        InitialCatalog = "DotNetTrainingBatch4",
+        // InitialCatalog = "TestDb",
+        UserID = "sa",
+        Password = "sasa@123",
+        TrustServerCertificate = true
+    };
+}
---
+++ DotNetTrainingBatch4.WinFormsAppSqlInjection/Form1.cs
@@ -0,0 +1,39 @@
+using DotNetTrainingBatch4.Shared;
+
+namespace DotNetTrainingBatch4.WinFormsAppSqlInjection
+{
+    public partial class Form1 : Form
+    {
+        private readonly DapperService _dapperService;
+
+        public Form1()
+        {
+            InitializeComponent();
+            _dapperService = new DapperService(ConnectionStrings.sqlConnectionStringBuilder.ConnectionString);
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            //string query = $"select * from tbl_user where email = '{txtEmail.Text.Trim()}' and password = '{txtPassword.Text.Trim()}'";
+            string query = $"select * from tbl_user where email = @Email and password = @Password";
+            var model = _dapperService.QueryFirstOrDefault<UserModel>(query, new
+            {
+                Email = txtEmail.Text.Trim(),
+                Password = txtPassword.Text.Trim()
+            });
+            if (model is null)
+            {
+                MessageBox.Show("User doesn't exist.");
+                return;
+            }
+
+            MessageBox.Show("Is Admin : " + model.Email);
+        }
+    }
+
+    public class UserModel
+    {
+        public string Email { get; set; }
+        public bool IsAdmin { get; set; }
+    }
+}
