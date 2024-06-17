ea858bd307b884e562f6f1d6a9436ace8ec74c4f
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2024-06-17 22:55:17 +0630

    [Complete] Minimal Api CRUD

---
+++ DotNetTrainingBatch4.MinimalApi/Features/Blog/BlogService.cs
@@ -0,0 +1,61 @@
+﻿using DotNetTrainingBatch4.MinimalApi.Db;
+using DotNetTrainingBatch4.MinimalApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotNetTrainingBatch4.MinimalApi.Features.Blog
+{
+    public static class BlogService
+    {
+        public static IEndpointRouteBuilder MapBlogs(this IEndpointRouteBuilder app)
+        {
+            app.MapGet("api/Blog", async (AppDbContext db) =>
+            {
+                var lst = await db.Blogs.AsNoTracking().ToListAsync();
+                return Results.Ok(lst);
+            });
+
+            app.MapPost("api/Blog", async (AppDbContext db, BlogModel blog) =>
+            {
+                await db.Blogs.AddAsync(blog);
+                var result = await db.SaveChangesAsync();
+
+                string message = result > 0 ? "Saving Successful." : "Saving Failed.";
+                return Results.Ok(message);
+            });
+
+            app.MapPut("api/Blog/{id}", async (AppDbContext db, int id, BlogModel blog) =>
+            {
+                var item = await db.Blogs.FirstOrDefaultAsync(x => x.BlogId == id);
+                if (item is null)
+                {
+                    return Results.NotFound("No data found.");
+                }
+
+                item.BlogTitle = blog.BlogTitle;
+                item.BlogAuthor = blog.BlogAuthor;
+                item.BlogContent = blog.BlogContent;
+                var result = await db.SaveChangesAsync();
+
+                string message = result > 0 ? "Updating Successful." : "Updating Failed.";
+                return Results.Ok(message);
+            });
+
+            app.MapDelete("api/Blog/{id}", async (AppDbContext db, int id) =>
+            {
+                var item = await db.Blogs.FirstOrDefaultAsync(x => x.BlogId == id);
+                if (item is null)
+                {
+                    return Results.NotFound("No data found.");
+                }
+
+                db.Blogs.Remove(item);
+                var result = await db.SaveChangesAsync();
+
+                string message = result > 0 ? "Deleting Successful." : "Deleting Failed.";
+                return Results.Ok(message);
+            });
+
+            return app;
+        }
+    }
+}
---
+++ DotNetTrainingBatch4.MinimalApi/Program.cs
@@ -0,0 +1,71 @@
+using DotNetTrainingBatch4.MinimalApi.Db;
+using DotNetTrainingBatch4.MinimalApi.Features.Blog;
+using DotNetTrainingBatch4.MinimalApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(opt =>
+{
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection"));
+},
+ServiceLifetime.Transient,
+ServiceLifetime.Transient);
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapGet("/", () => "Hello World");
+
+//BlogService.AddBlogFeatures(app);
+
+app.MapBlogs();
+
+app.Run();
+
+
+
+
+
+
+
+
+
+
+
+
+
+//var summaries = new[]
+//{
+//    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+//};
+
+//app.MapGet("/weatherforecast", () =>
+//{
+//    var forecast = Enumerable.Range(1, 5).Select(index =>
+//        new WeatherForecast
+//        (
+//            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+//            Random.Shared.Next(-20, 55),
+//            summaries[Random.Shared.Next(summaries.Length)]
+//        ))
+//        .ToArray();
+//    return forecast;
+//})
+//.WithName("GetWeatherForecast")
+//.WithOpenApi();
+
+//internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+//{
+//    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+//}
