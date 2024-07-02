2b348f376df2666b2512ca6c234fecff05fe3ab5
Su Sandar Lin <susandarlin.2016@gmail.com>
2024-07-03 00:56:59 +0700

    [Complete] ApexChart - ColumnChartWithDataLables

--- DotNetTrainingBatch4.MvcChartApp/Controllers/ApexChartController.cs
+++ DotNetTrainingBatch4.MvcChartApp/Controllers/ApexChartController.cs
@@ -13,5 +13,41 @@ namespace DotNetTrainingBatch4.MvcChartApp.Controllers
 
             return View(model);
         }
+
+        public IActionResult ColumnChartWithDataLables()
+        {
+            ColumnChartWithDataLablesModel model = new ColumnChartWithDataLablesModel();
+            model.Categories = new List<string>()
+            {
+                "Jan",
+                "Feb",
+                "Mar",
+                "Apr",
+                "May",
+                "Jun",
+                "Jul",
+                "Aug",
+                "Sep",
+                "Oct",
+                "Nov",
+                "Dec"
+            };
+            model.Inflation = new List<double>
+            {
+                2.3,
+                3.1,
+                4.0,
+                10.1,
+                4.0,
+                3.6,
+                3.2,
+                2.3,
+                1.4,
+                0.8,
+                0.5,
+                0.2
+            };
+            return View(model);
+        }
     }
 }
