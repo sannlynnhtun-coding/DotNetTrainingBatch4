3b221bee87eea9678b35bba6ef2f6c1af81b4916
paii29 <thetpainghein99@gmail.com>
2024-07-04 00:32:13 +0630

    [Complete] ApexChart - Custom Data Labels Bar Chart

--- DotNetTrainingBatch4.MvcChartApp/Controllers/ApexChartController.cs
+++ DotNetTrainingBatch4.MvcChartApp/Controllers/ApexChartController.cs
@@ -13,5 +13,23 @@ namespace DotNetTrainingBatch4.MvcChartApp.Controllers
 
             return View(model);
         }
+
+        public IActionResult CustomDataLabelsBarChart()
+        {
+            CustomDataLabelsBarChartModel model = new CustomDataLabelsBarChartModel();
+            model.Data = new List<int> { 400, 430, 448, 470, 540, 580, 690, 1100, 1200, 1380 };
+            model.Colors = new List<string>
+            {
+                "#33b2df", "#546E7A", "#d4526e", "#13d8aa", "#A5978B", "#2b908f", "#f9a3a4", "#90ee7e",
+            "#f48024", "#69d2e7"
+            };
+            model.Categories = new List<string>
+            {
+                "South Korea", "Canada", "United Kingdom", "Netherlands", "Italy", "France", "Japan",
+            "United States", "China", "India"
+            };
+            return View(model);
+        }
+
     }
 }
