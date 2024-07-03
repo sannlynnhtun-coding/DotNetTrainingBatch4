de8ff52c2c96ddb4e667ef7050b72c954edc379d
Wuttyi <ms.hninwuttyi@gmail.com>
2024-07-03 23:53:54 +0700

    [Complete] ApexChart - Basic Bar Chart

---
+++ DotNetTrainingBatch4.MvcChartApp/Models/BasicBarChartModel.cs
@@ -0,0 +1,8 @@
+﻿namespace DotNetTrainingBatch4.MvcChartApp.Models
+{
+    public class BasicBarChartModel
+    {
+        public List<int> Series { get; set; }
+        public List<string> Labels { get; set; }
+    }
+}
