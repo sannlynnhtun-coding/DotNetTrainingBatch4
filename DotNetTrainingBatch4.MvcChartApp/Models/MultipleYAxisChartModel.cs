6cd165f7b8a7736cf5c888d250480e0ec9cbed34
MratRikzil <142309328+MratRikzil@users.noreply.github.com>
2024-07-04 00:19:57 +0630

    Apex Chart (Multiple Y Axis Chart) [Complete]

---
+++ DotNetTrainingBatch4.MvcChartApp/Models/MultipleYAxisChartModel.cs
@@ -0,0 +1,16 @@
+﻿namespace DotNetTrainingBatch4.MvcChartApp.Models
+{
+    public class MultipleYAxisChartModel
+    {
+        public List<ListOfSeries>? Series { get; set; }
+        public List<int>? Categories { get; set; }
+    }
+
+    public class ListOfSeries 
+    {
+        public string? name { get; set; }
+        public string? type { get; set; }
+        public List<double>? data { get; set; }
+    }
+    
+}
