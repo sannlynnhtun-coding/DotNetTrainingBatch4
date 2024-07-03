5d0ab492f17ec3ff87fafbf53539045bf64a43ab
hewhounknown <zwemarnhtay@gmail.com>
2024-07-03 18:25:35 +0630

    [completed] ApexChart - Stacked Columns

--- DotNetTrainingBatch4.MvcChartApp/Controllers/ApexChartController.cs
+++ DotNetTrainingBatch4.MvcChartApp/Controllers/ApexChartController.cs
@@ -13,5 +13,32 @@ namespace DotNetTrainingBatch4.MvcChartApp.Controllers
 
             return View(model);
         }
+
+        public IActionResult StackedColumns()
+        {
+            StackedColumnModel model = new StackedColumnModel();
+            model.Series = new List<Series>()
+            {
+                new Series
+                {
+                    Name = "Product A",
+                    Data = new List<int> { 44, 55, 41, 67, 22, 43 }
+                },
+                new Series
+                {
+                    Name = "Product B",
+                    Data = new List<int> { 13, 23, 20, 8, 13, 27 }
+                },
+                new Series
+                {
+                    Name = "Product C",
+                    Data = new List<int> { 11, 17, 15, 15, 21, 14 }
+                }
+            };
+            model.Categories = new List<string>() { "01/01/2011 GMT", "01/02/2011 GMT", "01/03/2011 GMT", "01/04/2011 GMT",
+                    "01/05/2011 GMT", "01/06/2011 GMT"};
+
+            return View(model);
+        }
     }
 }
---
+++ DotNetTrainingBatch4.MvcChartApp/Models/StackedColumnModel.cs
@@ -0,0 +1,13 @@
+﻿namespace DotNetTrainingBatch4.MvcChartApp.Models;
+
+public class StackedColumnModel
+{
+    public List<Series> Series { get; set; }
+    public List<string> Categories { get; set; }
+}
+
+public class Series
+{
+    public string Name { get; set; }
+    public List<int> Data { get; set; }
+}
