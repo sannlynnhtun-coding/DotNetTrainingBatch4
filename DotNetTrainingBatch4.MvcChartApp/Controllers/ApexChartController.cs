1080259b9e0cccd657b610da4485b67bc6cc97ac
LinThitHtwe <linthit2745@gmail.com>
2024-07-03 13:19:07 +0630

    [Complete] Milti Series Timeline Chart

--- DotNetTrainingBatch4.MvcChartApp/Controllers/ApexChartController.cs
+++ DotNetTrainingBatch4.MvcChartApp/Controllers/ApexChartController.cs
@@ -13,5 +13,32 @@ namespace DotNetTrainingBatch4.MvcChartApp.Controllers
 
             return View(model);
         }
+
+        public IActionResult MultiSeriesTimelineChart()
+        {
+            var responseList = new List<MultiSeriesTimelineChartModel>
+        {
+            new()
+            {
+                Name = "Bob",
+                Timelines = new List<Timelines>
+                {
+                    new () { Title = "Design", DateTimes = new List<DateTime> { new (2019, 3, 5), new DateTime(2019, 3, 8) } },
+                    new () { Title = "Code", DateTimes = new List<DateTime> { new (2019, 3, 8), new DateTime(2019, 3, 11) } },
+                    new () { Title = "Test", DateTimes = new List<DateTime> { new (2019, 3, 11), new DateTime(2019, 3, 16) } }
+                }
+            },
+            new() {
+                Name = "Joe",
+                Timelines = new List<Timelines>
+                {
+                    new () { Title = "Design", DateTimes = new List<DateTime> { new (2019, 3, 2), new DateTime(2019, 3, 5) } },
+                    new () { Title = "Code", DateTimes = new List<DateTime> { new (2019, 3, 6), new DateTime(2019, 3, 9) } },
+                    new () { Title = "Test", DateTimes = new List<DateTime> { new (2019, 3, 10), new DateTime(2019, 3, 19) } }
+                }
+            }
+        };
+            return View(responseList);
+        }
     }
 }
