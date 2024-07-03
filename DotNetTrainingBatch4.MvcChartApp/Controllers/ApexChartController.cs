a44c3f06372ccb18eeccf5ec08929df3b9358789
Moe Yan Htun <moeyan227970@gmail.com>
2024-07-03 14:24:55 +0630

    [Complete] StackedBarChart

--- DotNetTrainingBatch4.MvcChartApp/Controllers/ApexChartController.cs
+++ DotNetTrainingBatch4.MvcChartApp/Controllers/ApexChartController.cs
@@ -13,5 +13,25 @@ namespace DotNetTrainingBatch4.MvcChartApp.Controllers
 
             return View(model);
         }
+
+        public IActionResult StackedBar()
+        {
+            var seriesData = new List<SeriesData>
+            {
+                new SeriesData { name = "Marine Sprite", data = new List<int> { 44, 55, 41, 37, 22, 43, 21 } },
+                new SeriesData { name = "Striking Calf", data = new List<int> { 53, 32, 33, 52, 13, 43, 32 } },
+                new SeriesData { name = "Tank Picture", data = new List<int> { 12, 17, 11, 9, 15, 11, 20 } },
+                new SeriesData { name = "Bucket Slope", data = new List<int> { 9, 7, 5, 8, 6, 9, 4 } },
+                new SeriesData { name = "Reborn Kid", data = new List<int> { 25, 12, 19, 32, 25, 24, 10 } }
+            };
+
+            var model = new BarChartModel
+            {
+                Series = seriesData,
+                Categories = new List<int> { 2008, 2009, 2010, 2011, 2012, 2013, 2014 }
+            };
+
+            return View(model);
+        }
     }
 }
---
+++ DotNetTrainingBatch4.MvcChartApp/Models/BarChartModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace DotNetTrainingBatch4.MvcChartApp.Models;
+
+public class BarChartModel
+{
+    public List<SeriesData> Series { get; set; }
+    public List<int> Categories { get; set; }
+}
+
+public class SeriesData
+{
+    public string name { get; set; }
+    public List<int> data { get; set; }
+}
+
