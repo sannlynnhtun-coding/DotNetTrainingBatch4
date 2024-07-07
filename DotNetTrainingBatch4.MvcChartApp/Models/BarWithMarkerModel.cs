d496dcf50e84af32c2d811f8622bba5817d3ed3d
Sandar Kyaw <dars7703@gmail.com>
2024-07-07 20:48:23 +0630

    [Complete]BarWithMarker

---
+++ DotNetTrainingBatch4.MvcChartApp/Models/BarWithMarkerModel.cs
@@ -0,0 +1,32 @@
+﻿namespace DotNetTrainingBatch4.MvcChartApp.Models
+{
+
+    public class GoalsList
+    {
+        public string Name { get; set; }
+        public int Value { get; set; }
+        public int StrokeWidth { get; set; }
+        public int StrokeHeight { get; set; }
+        public string StrokeDashArray { get; set; }
+        public string StrokeLineCap { get; set; }
+        public string StrokeColor { get; set; }
+    }
+
+    public class ExpecxtList
+    {
+        public string X { get; set; }
+        public int Y { get; set; }
+        public List<GoalsList> Goals { get; set; }
+    }
+
+    public class BarWithMarkerModel
+    {
+        public string Name { get; set; }
+        public List<ExpecxtList> Expect { get; set; }
+    }
+
+}
+
+
+
+
