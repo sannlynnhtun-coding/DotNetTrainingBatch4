c87347ec0214d50cb2217e8edd59d945c62079c4
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2024-07-08 22:28:42 +0630

    [Complete] CanvasJs

---
+++ DotNetTrainingBatch4.MvcChartApp/Controllers/CanvasJsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetTrainingBatch4.MvcChartApp.Controllers
+{
+    public class CanvasJsController : Controller
+    {
+        public IActionResult LineChart()
+        {
+            return View();
+        }
+    }
+}
