87b1fb15190c8100d4a03c4254a1e8c33092a0f5
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2024-07-08 22:47:00 +0630

    [Complete] Console App, ASP.NET Core Text Log

--- DotNetTrainingBatch4.MvcChartApp/Controllers/CanvasJsController.cs
+++ DotNetTrainingBatch4.MvcChartApp/Controllers/CanvasJsController.cs
@@ -4,8 +4,16 @@ namespace DotNetTrainingBatch4.MvcChartApp.Controllers
 {
     public class CanvasJsController : Controller
     {
+        private readonly ILogger<CanvasJsController> _logger;
+
+        public CanvasJsController(ILogger<CanvasJsController> logger)
+        {
+            _logger = logger;
+        }
+
         public IActionResult LineChart()
         {
+            _logger.LogInformation("Lin Chart...");
             return View();
         }
     }
