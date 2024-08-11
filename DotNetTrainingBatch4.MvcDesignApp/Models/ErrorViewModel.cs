bc5158f40d05ea7fe5dd86bee02356c423f67464
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2024-08-11 22:22:35 +0630

    feat: UI Design

---
+++ DotNetTrainingBatch4.MvcDesignApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DotNetTrainingBatch4.MvcDesignApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
