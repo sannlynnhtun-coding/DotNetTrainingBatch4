a811d3eb6fe083b8b29262886bbdaedabee655a0
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2024-08-19 23:01:44 +0630

    feat: blazor wasm + asp.net core web api (get, post)

---
+++ DotNetTrainingBatch4.BlazorWasm2/Models/BlogModel.cs
@@ -0,0 +1,10 @@
+﻿namespace DotNetTrainingBatch4.BlazorWasm2.Models
+{
+    public class BlogModel
+    {
+        public int BlogId { get; set; }
+        public string BlogTitle { get; set; }
+        public string BlogAuthor { get; set; }
+        public string BlogContent { get; set; }
+    }
+}
---
+++ DotNetTrainingBatch4.BlazorWasm2/Program.cs
@@ -0,0 +1,11 @@
+using DotNetTrainingBatch4.BlazorWasm2;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7230") });
+
+await builder.Build().RunAsync();
