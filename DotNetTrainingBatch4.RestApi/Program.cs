de65f89ada0a1f7d463cafd3848c08fdc004a4df
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2024-08-19 23:00:31 +0630

    feat: add cors

--- DotNetTrainingBatch4.RestApi/Program.cs
+++ DotNetTrainingBatch4.RestApi/Program.cs
@@ -1,6 +1,19 @@
 using Microsoft.EntityFrameworkCore;
 
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          policy.WithOrigins("https://localhost:7109",
+                                              "http://localhost:5214")
+                                .WithMethods("GET", "POST", "PUT", "PATCH", "DELETE")
+                                .AllowAnyHeader();
+                      });
+});
 
 // Add services to the container.
 
@@ -34,6 +47,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors(MyAllowSpecificOrigins);
 
 app.UseAuthorization();
 
